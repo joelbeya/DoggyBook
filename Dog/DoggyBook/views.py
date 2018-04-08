b38300b950690979e0ea4a28f8163d4ddebbebf6
fx <teiki.raihauti@etu.umontpellier.fr>
2018-04-08 21:58:55 +0200

    session

--- Dog/DoggyBook/views.py
+++ Dog/DoggyBook/views.py
@@ -6,52 +6,51 @@ from django.http import *
 from django.contrib.auth.hashers import *
 from datetime import datetime
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.models import User
+from django.contrib.auth import *
 
 
 def index(request):
     objets = Chien.objects.order_by('-created_at');
     return render(request, 'DoggyBook/index.html', {'objets':objets})
 
+
 def requete(request,obj):
     identifier = getattr(sys.modules[__name__], obj)
     objets = identifier.all()
     return render(request, 'DoggyBook/requete.html', {'objets':objets})
+
 
 def show(request, obj, key):
     identifier = getattr(sys.modules[__name__], obj)
     objet = identifier.find(int(key))
     return render(request, 'DoggyBook/show.html', {'objet':objet})
 
+
 def subscribe(request):
     mail = request.POST['mail']
-    password = make_password(request.POST['password'])
+    password = request.POST['password']
     nom = request.POST['name']
     prenom = request.POST['first_name']
     sexe = request.POST['sexe']
     date_naissance = request.POST['birth']
-    
-    User.objects.create(mail=mail, password=password, nom=nom, prenom=prenom, sexe=sexe, date_naissance=date_naissance)
-    return redirect('/DoggyBook/index')
+
+    User.objects.create_user(username=mail, email=mail, first_name=prenom, last_name=nom, password=password)
+
+    return redirect('/doggybook/index')
+
 
 def log(request):
-    try:
-        m = User.objects.get(mail=request.POST['mail'])
-    except ObjectDoesNotExist:
-        return HttpResponse("Your username and password didn't match.")
-
-    if check_password(request.POST['password'], m.password):
-        request.session['id'] = m.id
-        return redirect('/DoggyBook/index')
+    username = request.POST['mail']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('/doggybook/index')
     else:
         return HttpResponse("Your username and password didn't match.")
+
 
 def log_out(request):
-    request = HttpRequest()
-    engine = import_module(settings.SESSION_ENGINE)
-    if self.session:
-        request.session = self.session
-        request.user = get_user(request)
-    else:
-        request.session = engine.SessionStore()
     logout(request)
-    self.cookies = SimpleCookie() 
+    return redirect('/doggybook/index')
