fa4fbf4301ce25221cb3216e1ecd42dedc50f5b2
Your Name <ibrahima.tounkara@etu.umontpellier.fr>
2018-04-12 19:58:33 +0200

    Ajout de fichiers statiques

---
+++ Dog/DoggyBook/forms.py
@@ -0,0 +1,6 @@
+from .models import Photo
+
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ('file', )
--- Dog/DoggyBook/urls.py
+++ Dog/DoggyBook/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
 	url(r'^ajoutChien', views.ajoutChien),
 	url(r'^login', views.log),
 	url(r'^logout', views.log_out),
+	url(r'^basic-upload/$', views.BasicUploadView.as_view(), name='basic_upload'),
 	url(r'^utilisateur/(?P<key>[0-9]*)$', views.user),
     url(r'^(?P<obj>[A-Za-z]*)$', views.requete),
     url(r'^(?P<obj>[A-Za-z]*)/(?P<key>[0-9]*)$', views.show),
